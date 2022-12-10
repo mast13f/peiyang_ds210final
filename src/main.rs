b40b7edf6c39870b68b59ed0810685f26a155ae9
mast13f <nickpliu@bu.edu>
2022-12-10 01:59:57 -0500

    more dataset

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,37 @@
 //https://rust-lang-nursery.github.io/rust-cookbook/encoding/csv.html
 //
 
+mod some_func;
+// use crate::some_func::kmean_cluster;
+// use crate::some_func::draw;
+// use crate::some_func::vec_to_arr;
+use crate::some_func::*;
 
 use csv;
-use ndarray::Array2;
+use ndarray::{Array2, OwnedRepr};
 use std::error::Error;
 use serde::Deserialize;
 
 
-// Import the linfa prelude and KMeans algorithm
-use linfa::prelude::*;
-use linfa_clustering::KMeans;
-// We'll build our dataset on our own using ndarray and rand
+// // We'll build our dataset on our own using ndarray and rand
 use ndarray::prelude::*;
-use rand::prelude::*;
 // Import the plotters crate to create the scatter plot
 use plotters::prelude::*;
 
 
-#[derive(Debug, Deserialize)]
+ #[derive(Debug, Deserialize)]
 
 
 struct Stations {
     //X,Y,STATION,LINE,TERMINUS,ROUTE
     X: f32,
     Y: f32,
-    STATION: String,
-    LINE: String,
-    TERMINUS: String,
-    ROUTE: String,
+    NAME: String,
+
 }
 
 
-fn read_from_file(path: &str) -> Result<Vec<(f32, f32)>, Box<dyn Error>>{
+fn read_csv_file(path: &str) -> Result<Vec<(f32, f32)>, Box<dyn Error>>{
     // get the x and y coordination of stations
     // from cvs file to a vector
     let mut locations: Vec<(f32, f32)> = Vec::new();
@@ -48,115 +47,41 @@ fn read_from_file(path: &str) -> Result<Vec<(f32, f32)>, Box<dyn Error>>{
 }
 
 
-fn vec_to_arr(vector: Vec<(f32,f32)>) -> Array2<f32>{
-    // transfer vectors to 2d array in order to conduct k mean
-    let mut result = Array2::<f32>::zeros((vector.len(), 2));
-    for i in 0..vector.len(){
-        for u in 0..2{
-            if u == 0{
-                result[[i,u]]= vector[i].0
-            }else{
-                result[[i,u]]= vector[i].1
-            }
-        }
-    }
-    result
-}
-
-
 fn main() {
-    if let Err(e) = read_from_file("./MBTA_NODE.csv"){
+
+    // data of mbta stations:
+    if let Err(e) = read_csv_file("./MBTA_NODE.csv"){
         eprintln!("{}", e);
     }else {
-        let _result = read_from_file("./MBTA_NODE.csv");
+        let _result = read_csv_file("./MBTA_NODE.csv");
     }
-    let result = read_from_file("./MBTA_NODE.csv");
+    let result = read_csv_file("./MBTA_NODE.csv");
     let data  = result.unwrap();
     let data_arr = vec_to_arr(data);
-    //println!("{:?}",data_arr);
 
-    let dataset = DatasetBase::from(data_arr);
-    let rng = thread_rng(); // Random number generator
-    let n_clusters = 5;
-    let model = KMeans::params_with_rng(n_clusters, rng)
-    .max_n_iterations(100)
-    .tolerance(1e-5)
-    .fit(&dataset)
-    .expect("Error while fitting KMeans to the dataset");
-
-    let dataset = model.predict(dataset);
-    println!("{:?}", dataset.records);
-    println!("{:?}", dataset.targets);
+    let dataset = kmean_cluster(data_arr,100);
+    draw("MBTA.png","MBTA K-Mean Cluster",dataset);
 
 
 
-    let root = BitMapBackend::new("kmeans.png", (1000, 1000)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
 
-    let x_lim = 215000.0..250000.0f32;
-    let y_lim = 880000.0..915000.0f32;
+    // data of colleges:
+    if let Err(e) = read_csv_file("./COLLEGES_PT.csv"){
+        eprintln!("{}", e);
+    }else {
+        let _result_c = read_csv_file("./COLLEGES_PT.csv");
+    }
+    let result_c = read_csv_file("./COLLEGES_PT.csv");
+    let data_c  = result_c.unwrap();
+    let data_c_arr = vec_to_arr(data_c);
 
-    let mut ctx = ChartBuilder::on(&root)
-    .set_label_area_size(LabelAreaPosition::Left, 40) // Put in some margins
-    .set_label_area_size(LabelAreaPosition::Right, 40)
-    .set_label_area_size(LabelAreaPosition::Bottom, 40)
-    .caption("KMeans Clustering of MBTA Stations", ("sans-serif", 25)) // Set a caption and font
-    .build_cartesian_2d(x_lim, y_lim)
-    .expect("Couldn't build our ChartBuilder");
-
-    ctx.configure_mesh().draw().unwrap();
-    let root_area = ctx.plotting_area();
-
-    // check_array_for_plotting(dataset: &Array2<f32>) -> bool {}
-    //check_array_for_plotting(&dataset.records); // Panics if that's not true
+    let dataset_c = kmean_cluster(data_c_arr,100);
+    draw("Colleges.png","Colleges K-Mean Cluster",dataset_c)
 
 
-    for i in 0..dataset.records.shape()[0] {
-    let coordinates = dataset.records.slice(s![i, 0..2]);
 
-    let point = match dataset.targets[i] {
-    0 => Circle::new(
-    (coordinates[0], coordinates[1]),
-    3,
-    ShapeStyle::from(&RGBColor(0, 153, 153)).filled(),
-    ),
 
-    1 => Circle::new(
-    (coordinates[0], coordinates[1]),
-    3,
-    ShapeStyle::from(&RGBColor(153, 0, 153)).filled(),
-    ),
 
-    2 => Circle::new(
-    (coordinates[0], coordinates[1]),
-    3,
-    ShapeStyle::from(&RGBColor(102, 204, 0)).filled(),
-    ),
-
-    3 => Circle::new(
-    (coordinates[0], coordinates[1]),
-    3,
-    ShapeStyle::from(&RGBColor(255, 51, 51)).filled(),
-    ),
-
-    4 => Circle::new(
-    (coordinates[0], coordinates[1]),
-    3,
-    ShapeStyle::from(&RGBColor(210, 180, 0)).filled(),
-    ),
-
-    // Making sure our pattern-matching is exhaustive
-    _ => Circle::new(
-    (coordinates[0], coordinates[1]),
-    3,
-    ShapeStyle::from(&BLACK).filled(),
-    ),
-    };
-
-    root_area
-    .draw(&point)
-    .expect("An error occurred while drawing the point!");
-}
 }
 
 
---
+++ src/some_func.rs
@@ -0,0 +1,118 @@
+use csv;
+use ndarray::{Array2, OwnedRepr};
+use std::error::Error;
+use serde::Deserialize;
+
+
+// Import the linfa prelude and KMeans algorithm
+use linfa::prelude::*;
+use linfa_clustering::KMeans;
+// We'll build our dataset on our own using ndarray and rand
+use ndarray::prelude::*;
+use rand::prelude::*;
+// Import the plotters crate to create the scatter plot
+use plotters::prelude::*;
+
+
+pub fn vec_to_arr(vector: Vec<(f32,f32)>) -> Array2<f32>{
+    // transfer vectors to 2d array in order to conduct k mean
+    let mut result = Array2::<f32>::zeros((vector.len(), 2));
+    for i in 0..vector.len(){
+        for u in 0..2{
+            if u == 0{
+                result[[i,u]]= vector[i].0
+            }else{
+                result[[i,u]]= vector[i].1
+            }
+        }
+    }
+    result
+}
+
+pub fn kmean_cluster(input_data: Array2<f32>, n_iter:usize ) -> DatasetBase<ArrayBase<OwnedRepr<f32>, Ix2>, Array1<usize>> {
+
+    let dataset = DatasetBase::from(input_data);
+    let rng = thread_rng(); // Random number generator
+    let n_clusters = 5;
+    let model = KMeans::params_with_rng(n_clusters, rng)
+    .max_n_iterations(100)
+    .tolerance(1e-5)
+    .fit(&dataset)
+    .expect("Error while fitting KMeans to the dataset");
+
+    let dataset = model.predict(dataset);
+    println!("{:?}", dataset.records);
+    println!("{:?}", dataset.targets);
+    dataset
+}
+
+pub fn draw(pngname: &str, title:&str, data:DatasetBase<ArrayBase<OwnedRepr<f32>, Ix2>, Array1<usize>>){
+    let root = BitMapBackend::new(pngname, (1000, 1000)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let x_lim = 215000.0..250000.0f32;
+    let y_lim = 880000.0..915000.0f32;
+
+    let mut ctx = ChartBuilder::on(&root)
+    .set_label_area_size(LabelAreaPosition::Left, 40) // Put in some margins
+    .set_label_area_size(LabelAreaPosition::Right, 40)
+    .set_label_area_size(LabelAreaPosition::Bottom, 40)
+    .caption(title, ("sans-serif", 25)) // Set a caption and font
+    .build_cartesian_2d(x_lim, y_lim)
+    .expect("Couldn't build our ChartBuilder");
+
+    ctx.configure_mesh().draw().unwrap();
+    let root_area = ctx.plotting_area();
+
+    // check_array_for_plotting(dataset: &Array2<f32>) -> bool {}
+    //check_array_for_plotting(&dataset.records); // Panics if that's not true
+
+
+    for i in 0..data.records.shape()[0] {
+    let coordinates = data.records.slice(s![i, 0..2]);
+
+    let point = match data.targets[i] {
+    0 => Circle::new(
+    (coordinates[0], coordinates[1]),
+    3,
+    ShapeStyle::from(&RGBColor(0, 153, 153)).filled(),
+    ),
+
+    1 => Circle::new(
+    (coordinates[0], coordinates[1]),
+    3,
+    ShapeStyle::from(&RGBColor(153, 0, 153)).filled(),
+    ),
+
+    2 => Circle::new(
+    (coordinates[0], coordinates[1]),
+    3,
+    ShapeStyle::from(&RGBColor(102, 204, 0)).filled(),
+    ),
+
+    3 => Circle::new(
+    (coordinates[0], coordinates[1]),
+    3,
+    ShapeStyle::from(&RGBColor(255, 51, 51)).filled(),
+    ),
+
+    4 => Circle::new(
+    (coordinates[0], coordinates[1]),
+    3,
+    ShapeStyle::from(&RGBColor(210, 180, 0)).filled(),
+    ),
+
+    // Making sure our pattern-matching is exhaustive
+    _ => Circle::new(
+    (coordinates[0], coordinates[1]),
+    3,
+    ShapeStyle::from(&BLACK).filled(),
+    ),
+    };
+
+    root_area
+    .draw(&point)
+    .expect("An error occurred while drawing the point!");
+}
+
+}
