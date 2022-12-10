58e14222766357655699fcc17f001d80c6fe3d9b
mast13f <nickpliu@bu.edu>
2022-12-09 20:59:26 -0500

    add data from csv file to vec

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 
-
-use csv::{ReaderBuilder, WriterBuilder};
-use ndarray::{Array, Array2};
-use ndarray_csv::{Array2Reader, Array2Writer};
+use csv;
+use ndarray::Array2;
 use std::error::Error;
-use std::fs::File;
-
-use std::io;
-use std::process;
-
-//use kmeans::*;
 use serde::Deserialize;
 
 
-
 #[derive(Debug, Deserialize)]
+
+
 struct Stations {
     //X,Y,STATION,LINE,TERMINUS,ROUTE
     X: f32,
@@ -24,68 +17,51 @@ struct Stations {
     TERMINUS: String,
     ROUTE: String,
 }
-fn read_from_file(path: &str) -> Result<Vec<(f32, f32)>, Box<dyn Error>>{
-    let mut locations: Vec<(f32, f32)> = Vec::new();
+
+
+fn read_from_file(path: &str) -> Result<Vec<(f32, f32,String)>, Box<dyn Error>>{
+    // get the x and y coordination of stations
+    // from cvs file to a vector
+    let mut locations: Vec<(f32, f32, String)> = Vec::new();
     let mut rdr = csv::Reader::from_path(path)?;
 
     for result in rdr.deserialize() {
         let record: Stations = result?;
-        locations.push((record.X,record.Y));
+        locations.push((record.X,record.Y,record.LINE));
 
     }
-
     Ok(locations)
 }
 
-// fn read_csv() -> Result<(Array2<f32>), Box<dyn Error>> {
-//     // Our 2x3 test array
-//
-//     // Read an array back from the file
-//     let file = File::open("MBTA_NODE.csv")?;
-//     let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
-//     let array_read: Array2<f32> = reader.deserialize_array2((164, 5))?;
-//
-//     Ok((array_read))
-// }
 
-fn vec_to_arr(vector: Vec<(f32,f32)>) -> Array2<f32>{
-    let mut result = Array2::<f32>::zeros((vector.len(), 2));
+fn vec_to_arr(vector: Vec<(f32,f32,String)>) -> Array2<f32>{
+    // transfer vectors to 2d array in order to conduct k mean
+    let mut result = Array2::<f32>::zeros((vector.len(), 3));
     for i in 0..vector.len(){
-        for u in 0..2{
-            result[[i,u]]= vector[i].
-
+        for u in 0..3{
+            if u == 0{
+                result[[i,u]]= vector[i].0
+            }else{
+                result[[i,u]]= vector[i].1
+            }
         }
     }
     result
 }
 
 
-
-
 fn main() {
     if let Err(e) = read_from_file("./MBTA_NODE.csv"){
         eprintln!("{}", e);
     }else {
-        let mut result = read_from_file("./MBTA_NODE.csv");
-
+        let _result = read_from_file("./MBTA_NODE.csv");
     }
-    let mut result = read_from_file("./MBTA_NODE.csv");
-    let mut data  = result.unwrap();
-    let mut data_arr = vec_to_arr(data);
+    let result = read_from_file("./MBTA_NODE.csv");
+    let data  = result.unwrap();
+    let data_arr = vec_to_arr(data);
     println!("{:?}",data_arr);
 
 }
 
 
-// use polars::prelude::*;
-// use std::fs::File;
-//
-// fn example() -> Result<DataFrame, E> {
-//     let file = File::open("MBTA_NODE.csv").expect("could not open file");
-//
-//     CsvReader::new(file)
-//             .infer_schema(None)
-//             .has_header(true)
-//             .finish()
-// }
 
