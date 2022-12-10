04f2bf07be9abcd03262905a3de7217c1f5d0bc1
mast13f <nickpliu@bu.edu>
2022-12-09 20:31:27 -0500

    add data from csv file to vec

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
+
+
+use csv::{ReaderBuilder, WriterBuilder};
+use ndarray::{Array, Array2};
+use ndarray_csv::{Array2Reader, Array2Writer};
 use std::error::Error;
+use std::fs::File;
+
 use std::io;
 use std::process;
 
+//use kmeans::*;
 use serde::Deserialize;
+
+
 
 #[derive(Debug, Deserialize)]
 struct Stations {
     //X,Y,STATION,LINE,TERMINUS,ROUTE
-    X: f64,
-    Y: f64,
+    X: f32,
+    Y: f32,
     STATION: String,
     LINE: String,
     TERMINUS: String,
     ROUTE: String,
 }
-fn read_from_file(path: &str) -> Result<Vec<(f64, f64)>, Box<dyn Error>>{
-    let mut locations: Vec<(f64, f64)> = Vec::new();
+fn read_from_file(path: &str) -> Result<Vec<(f32, f32)>, Box<dyn Error>>{
+    let mut locations: Vec<(f32, f32)> = Vec::new();
     let mut rdr = csv::Reader::from_path(path)?;
 
     for result in rdr.deserialize() {
@@ -27,17 +37,42 @@ fn read_from_file(path: &str) -> Result<Vec<(f64, f64)>, Box<dyn Error>>{
     Ok(locations)
 }
 
+// fn read_csv() -> Result<(Array2<f32>), Box<dyn Error>> {
+//     // Our 2x3 test array
+//
+//     // Read an array back from the file
+//     let file = File::open("MBTA_NODE.csv")?;
+//     let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
+//     let array_read: Array2<f32> = reader.deserialize_array2((164, 5))?;
+//
+//     Ok((array_read))
+// }
+
+fn vec_to_arr(vector: Vec<(f32,f32)>) -> Array2<f32>{
+    let mut result = Array2::<f32>::zeros((vector.len(), 2));
+    for i in 0..vector.len(){
+        for u in 0..2{
+            result[[i,u]]= vector[i].
+
+        }
+    }
+    result
+}
+
+
+
 
 fn main() {
-    // if let Err(e) = read_from_file("./MBTA_NODE.csv"){
-    //     eprintln!("{}", e);
-    // }else {
-    //     let mut result = read_from_file("./MBTA_NODE.csv");
-    //
-    // }
+    if let Err(e) = read_from_file("./MBTA_NODE.csv"){
+        eprintln!("{}", e);
+    }else {
+        let mut result = read_from_file("./MBTA_NODE.csv");
+
+    }
     let mut result = read_from_file("./MBTA_NODE.csv");
     let mut data  = result.unwrap();
-    println!("{:?}",data)
+    let mut data_arr = vec_to_arr(data);
+    println!("{:?}",data_arr);
 
 }
 
